fa00e624af6314b1a9844b28e7d7e8fd9eb20de3
minhnhat09 <nhatminh1809@gmail.com>
2015-06-04 14:42:29 +0200

    Fix notification bug

--- www/app/home/notis-ctrl.js
+++ www/app/home/notis-ctrl.js
@@ -1,17 +1,23 @@
 (function (){
 	'use strict';
 	angular.module('eliteApp')
-	.controller('NotisCtrl', ['$scope', '$state', 'forumApi', NotisCtrl]);
+	.controller('NotisCtrl', ['$scope', '$state', 'forumApi', 'notisApi', NotisCtrl]);
 
-	function NotisCtrl($scope, $state, forumApi){
+	function NotisCtrl($scope, $state, forumApi, notisApi){
 
 		var vm = this;
 		console.log(forumApi);
 		console.log("Page notis, current user is " + forumApi.getCurrentUser());
+
+		notisApi.getNotisByCurrentUser().then(function(data){
+			vm.notis = data;
+			console.log("Reminders " + data);
+		});
+
+		notisApi.getMessagesByCurrentUser().then(function(data){
+			vm.messages = data;
+			console.log("messages " + data);
+		});
 		
-		vm.selectForum = function(id){
-			forumApi.setForumId(id);
-			$state.go("app.forum-detail");
-		}
 	}
 })();
